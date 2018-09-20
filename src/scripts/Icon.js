a5756b963285cfb05101fd53f6710b8d902d98bc
Mateo Murphy <mateo.murphy@gmail.com>
2018-09-20 17:18:17 -0400

    Remove svg4everybody dependency

--- src/scripts/Icon.js
+++ src/scripts/Icon.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import svg4everybody from 'svg4everybody';
 import { registerStyle, getAssetRoot } from './util';
-
-svg4everybody();
 
 /* eslint-disable max-len */
 const STANDARD_ICONS = `
